1bfee04acdeb73968417bc778b919f9625463863
James Nzuki <nzukijames200@gmail.com>
2026-07-30 12:29:41 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { GameStateProvider } from './context/GameStateContext';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <StrictMode>
+    <GameStateProvider>
+      <App />
+    </GameStateProvider>
+  </StrictMode>,
+);
