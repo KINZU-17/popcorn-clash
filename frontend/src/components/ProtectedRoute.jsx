d6cfa8c8c350c487dca25c591ccc3c346727a3b9
James Nzuki <nzukijames200@gmail.com>
2026-07-30 12:50:27 +0300

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useGame } from '../context/GameStateContext';
 
 export const ProtectedRoute = () => {
-  const gameContext = useGame();
-  const { user } = gameContext || {};
-  return user?.isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  const { user } = useGame();
+  return user.isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
